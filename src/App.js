cffd12650b159b2f75868ea16d52fb3af16a5a13
thebadteacher <stanis.samisco@gmail.com>
2020-06-15 22:49:30 +0200

    Finished signIn component

--- src/App.js
+++ src/App.js
@@ -41,8 +41,6 @@ class App extends React.Component {
 							...snapShot.data(),
 						},
 					});
-
-					console.log(this.state);
 				});
 			} else {
 				this.setState({ currentUser: userAuth });
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -23,8 +23,18 @@ class SignIn extends React.Component {
 		this.setState({ [name]: value });
 	};
 
-	handleSubmit = (e) => {
+	handleSubmit = async (e) => {
 		e.preventDefault();
+
+		const { email, password } = this.state;
+
+		try {
+			await auth.signInWithEmailAndPassword(email, password);
+
+			this.setState({ email: '', password: '' });
+		} catch (error) {
+			console.log(error);
+		}
 
 		this.setState({ email: '', password: '' });
 	};
